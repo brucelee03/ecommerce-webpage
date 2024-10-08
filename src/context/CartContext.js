248face514bd12bbaa039a7f5c40718bb9e30631
Rohit Prajapati <117084718+brucelee03@users.noreply.github.com>
2024-10-08 19:21:54 +0530

    Update CartContext.js
    Add some extra functions to use of context easy.

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const CartContext = React.createContext({
   cartList: [],
+  removeAllCartItems: () => {},
   addCartItem: () => {},
-  deleteCartItem: () => {},
-  removeAllItems: () => {},
+  removeCartItem: () => {},
+  incrementCartItemQuantity: () => {},
+  decrementCartItemQuantity: () => {},
 })
 
 export default CartContext
