0814f003fd9cd5b4cc74b59bb0cb3efbb286a248
Rohit Prajapati <117084718+brucelee03@users.noreply.github.com>
2024-10-08 19:23:15 +0530

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,31 +1,28 @@
-import CartContext from '../../context/CartContext'
-import EmptyCartView from '../EmptyCartView'
 import Header from '../Header'
 import CartListView from '../CartListView'
+
+import CartContext from '../../context/CartContext'
+import CartSummary from '../CartSummary'
+import EmptyCartView from '../EmptyCartView'
 
 import './index.css'
 
 const Cart = () => (
   <CartContext.Consumer>
     {value => {
-      const {cartList, removeAllItems} = value
+      const {cartList, removeAllCartItems} = value
       const showEmptyView = cartList.length === 0
-      let totalOrderAmount = 0
-      totalOrderAmount = cartList
-        .map(obj => obj.price * obj.quantity)
-        .reduce((acc, current) => acc + current, 0)
-
       const onRemoveAllItem = () => {
-        removeAllItems()
+        removeAllCartItems()
       }
 
       return (
         <>
           <Header />
-          {showEmptyView ? (
-            <EmptyCartView />
-          ) : (
-            <div className="cart-container">
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
               <div className="cart-content-container">
                 <div className="cart-content-header">
                   <h1 className="cart-heading">My Cart</h1>
@@ -38,23 +35,10 @@ const Cart = () => (
                   </button>
                 </div>
                 <CartListView />
-                <div className="total-order-amount-card">
-                  <h1 className="total-order-amount">
-                    Order Total:{' '}
-                    <span className="order-amount">
-                      Rs {totalOrderAmount}/-
-                    </span>
-                  </h1>
-                  <p className="total-order-items">
-                    {cartList.length} Items in cart
-                  </p>
-                  <button type="button" className="checkout-btn">
-                    Checkout
-                  </button>
-                </div>
+                <CartSummary />
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </>
       )
     }}
